ae393ebe637bcd30675fb5fdb4d30f2ed0d86d04
Chase Ehlert <chaseehlert123@gmail.com>
2023-02-24 15:42:58 -0500

    adding missing env import

--- src/commands/alert.js
+++ src/commands/alert.js
@@ -1,4 +1,5 @@
 import { SlashCommandBuilder } from "discord.js"
+import 'dotenv/config'
 
 export default {
     data: new SlashCommandBuilder()
@@ -7,7 +8,7 @@ export default {
     async execute(interaction) {
         const authorizeMessage = {
             title: 'Authorize D2VendorAlert',
-            url: `https://www.bungie.net/en/oauth/authorize?client_id=${VENDOR_ALERT_OAUTH_CLIENT_ID}&response_type=code`
+            url: `https://www.bungie.net/en/oauth/authorize?client_id=${process.env.VENDOR_ALERT_OAUTH_CLIENT_ID}&response_type=code`
         }
         await interaction.reply(authorizeMessage)
     }
