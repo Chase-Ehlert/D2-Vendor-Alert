21544d6bafb3fcfe30ded1d505a647b6a0fbf93a
Chase Ehlert <chaseehlert123@gmail.com>
2023-03-13 20:59:13 -0400

    direcorty reorg and testing landing page

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import 'dotenv/config'
 import express from 'express'
 import * as database from './database/users-operations.js'
 import { setupDiscordClient } from './discord/discord-client.js'
-import { handleRefreshToken } from './database/index.js'
+import { handleRefreshToken } from './database/refresh-token.js'
+import path from 'path'
 
 const app = express()
+const directoryName = path.dirname('app.js')
 
 database.setupDatabaseConnection()
 setupDiscordClient()
@@ -16,5 +18,6 @@ app.listen(3001, () => {
 app.get('/', async (request, result) => {
   handleRefreshToken(request)
 
-  result.send('YAY')
+  // result.send('YAY')
+  result.sendFile('views/landing-page.html', { root: directoryName })
 })
