433c36e9c07ae26397f8dc4d7496c116e7b6079c
Chase Ehlert <chaseehlert123@gmail.com>
2023-03-24 16:14:09 -0400

    debugging username check

--- src/discord/discord-client.js
+++ src/discord/discord-client.js
@@ -77,9 +77,7 @@ async function replyToSlashCommands(discordClient) {
                 console.log('DATA')
                 console.log(data)
                 console.log('HELLO')
-                console.log(typeof(data))
-                const object = JSON.parse(data)
-                console.log(object.Response)
+                console.log(data.Response)
 
                 data['Response'].size === 0 ? console.log('true') : console.log('false')
 
