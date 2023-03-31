5b6a82d802a429eec13c653f274fc461d97f83fa
Chase Ehlert <chaseehlert123@gmail.com>
2023-03-31 13:26:34 -0400

    debugging call to refresh destiny api token

--- src/utilities/vendor-utils.js
+++ src/utilities/vendor-utils.js
@@ -85,12 +85,12 @@ async function getOauthJson(refreshToken) {
   console.log('GUESSING')
 
   // post call is failing
-  const getOauthCredentials = await axios.post('https://www.bungie.net/platform/app/oauth/token/', {
+  const getOauthCredentials = await axios.post('https://www.bungie.net/platform/app/oauth/token/', new URLSearchParams({
     grant_type: 'refresh_token',
     refresh_token: refreshToken,
     client_id: process.env.VENDOR_ALERT_OAUTH_CLIENT_ID,
     client_secret: process.env.VENDOR_ALERT_OAUTH_SECRET
-  }, {
+  }), {
     headers: {
       'x-api-key': `${process.env.VENDOR_ALERT_API_KEY}`
     }
