52771f32889216125f30f51c1cb2a6280e7c5942
Chase Ehlert <chaseehlert123@gmail.com>
2023-04-04 20:02:45 -0400

    debugging manifest response

--- src/utilities/manifest-utils.js
+++ src/utilities/manifest-utils.js
@@ -19,7 +19,7 @@ export async function getItemFromManifest(itemType, itemList) {
      )
     // .then(async data => {
       console.log('DOG')
-      console.log(typeof(response.data))
+      console.log(response.data)
       inventoryNameList = await readItemsFromManifest(
         itemType,
         itemManifestFileName,
