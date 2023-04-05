19d7a66d5e917c052deab19bbd6694e60107de19
Chase Ehlert <chaseehlert123@gmail.com>
2023-04-04 20:11:22 -0400

    debugging manifest response

--- src/utilities/manifest-utils.js
+++ src/utilities/manifest-utils.js
@@ -19,7 +19,7 @@ export async function getItemFromManifest(itemType, itemList) {
      )
     // .then(async data => {
       console.log('DOG')
-      console.log(JSON.parse(response.data.toString().DestinyInventoryItemDefinition))
+      console.log(JSON.parse(response.data.toString()).DestinyInventoryItemDefinition)
       inventoryNameList = await readItemsFromManifest(
         itemType,
         itemManifestFileName,
