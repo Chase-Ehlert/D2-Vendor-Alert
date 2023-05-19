2c25bc14198a522c02a984a2301fc4e0b610c8c4
Chase Ehlert <chaseehlert123@gmail.com>
2023-05-19 15:24:54 -0400

    finished jsdocs for discord-utils

--- src/utilities/discord-utils.js
+++ src/utilities/discord-utils.js
@@ -4,6 +4,9 @@ import { User } from '../database/models/users.js'
 import { getProfileCollectibles } from './vendor-utils.js'
 import { updateRefreshToken } from './token-utils.js'
 
+/**
+ * Alert registered users about today's vendor inventory
+ */
 export async function sendMessage() {
   for await (const user of User.find()) {
     const discordEndpoint = `channels/${user.discord_channel_id}/messages`
@@ -21,6 +24,11 @@ export async function sendMessage() {
   }
 }
 
+/**
+ * Check whether any mods for sale are owned by the user
+ * @param {User} user User's profile information
+ * @param {string} discordEndpoint Endpoint for user's desired alert Discord channel
+ */
 async function compareModListWithUserInventory(user, discordEndpoint) {
     const unownedModList = await getProfileCollectibles(user)
     if (unownedModList.length > 0) {
@@ -30,6 +38,12 @@ async function compareModListWithUserInventory(user, discordEndpoint) {
     }
 }
 
+/**
+ * Send alert message for unowned mods
+ * @param {string} discordEndpoint Endpoint for user's desired alert Discord channel
+ * @param {string} discordId User's Discord id
+ * @param {Array<string>} unownedModList List of mods unowned by the user
+ */
 async function shareUnownedModsList(discordEndpoint, discordId, unownedModList) {
   let message = `<@${discordId}>\r\nYou have these unowned mods for sale, grab them!`
 
@@ -37,16 +51,26 @@ async function shareUnownedModsList(discordEndpoint, discordId, unownedModList) 
     message = message + `\r\n${mod}`
   })
 
-  await DiscordRequest(discordEndpoint, message)
+  await discordRequest(discordEndpoint, message)
 }
 
+/**
+ * Send update message for no alert required
+ * @param {string} discordEndpoint Endpoint for user's desired alert Discord channel
+ * @param {string} username User's Bungie username
+ */
 async function shareEmptyModsList(discordEndpoint, username) {
   let message = `${username} does not have any unowned mods for sale today.`
 
-  await DiscordRequest(discordEndpoint, message)
+  await discordRequest(discordEndpoint, message)
 }
 
-export async function DiscordRequest(endpoint, message) {
+/**
+ * Send off message to user's desired Discord alert channel
+ * @param {string} endpoint Endpoint for user's desired alert Discord channel
+ * @param {string} message Message to send to user
+ */
+export async function discordRequest(endpoint, message) {
   const result = await axios.post('https://discord.com/api/v10/' + endpoint,
     {
       "content": message
@@ -62,6 +86,4 @@ export async function DiscordRequest(endpoint, message) {
   if (result.status != 200) {
     throw new Error(result.status)
   }
-
-  return result
 }
