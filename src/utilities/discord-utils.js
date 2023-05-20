106e2363cb24fdca346ad72cc34c4749f6baec82
Chase Ehlert <chaseehlert123@gmail.com>
2023-05-20 14:31:16 -0400

    testing users bungie username

--- src/utilities/discord-utils.js
+++ src/utilities/discord-utils.js
@@ -34,6 +34,8 @@ async function compareModListWithUserInventory(user, discordEndpoint) {
     if (unownedModList.length > 0) {
       await shareUnownedModsList(discordEndpoint, user.discord_id, unownedModList)
     } else {
+      console.log(`EMPTY MOD LIST USERNAME ${user.bungie_username}`)
+      console.log(user)
       await shareEmptyModsList(discordEndpoint, user.bungie_username)
     }
 }
