166adca46576981b029de069c11b81f52c73ec10
Chase Ehlert <chaseehlert123@gmail.com>
2023-06-02 16:16:02 -0400

    refactored vendor-util jsdoc

--- src/utilities/vendor-utils.js
+++ src/utilities/vendor-utils.js
@@ -7,9 +7,9 @@ import { updateRefreshToken } from './token-utils.js'
 
 /**
  * Collect mods for a specific vendor
- * @param {string} vendorId id of vendor
- * @param {Object} user user profile information
- * @returns {Promise<Array<string>>} list of mods for sale
+ * @param {string} vendorId Id of vendor
+ * @param {Object} user User profile information
+ * @returns {Promise<Array<string>>} List of mods for sale
  */
 export async function getVendorModInventory(vendorId, user) {
   const oauthToken = await updateRefreshToken(user.refresh_token)
@@ -36,22 +36,9 @@ export async function getVendorModInventory(vendorId, user) {
 }
 
 /**
- * User profile information
- * @typedef {Object} User user profile information
- * @property {string} bungie_username user's Bungie username
- * @property {string} discord_id user's id on Discord
- * @property {string} discord_channel_id Discord channel id from user's slash command execution
- * @property {string} bungie_membership_id user's membership id on Bungie
- * @property {string} destiny_id user's id in Destiny 2
- * @property {string} destiny_character_id user's character (Hunter, Titan, Warlock) id
- * @property {string} refresh_expiration date of expiration for user's refresh token
- * @property {string} refresh_token user's refresh token
-*/
-
-/**
  * Collect mods for sale by Banshee-44 and Ada-1
- * @param {User} user user information
- * @returns {Promise<Array>} list of mods
+ * @param {Object} user User information
+ * @returns {Promise<Array>} List of mods
  */
 export async function getProfileCollectibles(user) {
   const profileResponse = await axios.get(`https://www.bungie.net/Platform/Destiny2/3/Profile/${user.destiny_id}/`, {
