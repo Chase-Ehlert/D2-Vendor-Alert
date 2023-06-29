3181927b43b518970627eac4a13f7a6c87cebd7e
Chase Ehlert <chaseehlert123@gmail.com>
2023-06-29 13:43:13 -0400

    fixing render file for landing page

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ app.get('/', (async (request, result) => {
     console.log(request)
     const guardian = await handleAuthorizationCode(String(request.query.code))
 
-    result.render('src/views/landing-page.html', { guardian })
+    result.render('landing-page.mustache', { guardian })
     // result.sendFile('src/views/landing-page.html', { root: directoryName })
   }
 }) as express.RequestHandler)
