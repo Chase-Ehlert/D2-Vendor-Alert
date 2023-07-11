be45300ad83e9305dba75cad1345ad9c1e2e821c
Chase Ehlert <chaseehlert123@gmail.com>
2023-07-11 10:18:27 -0400

    removing dist directory

--- dist/src/services/models/refresh-token-info.js
+++
@@ -1,9 +0,0 @@
-export class RefreshTokenInfo {
-    constructor(bungieMembershipId, refreshTokenExpirationTime, refreshToken, accessToken) {
-        this.bungieMembershipId = bungieMembershipId;
-        this.refreshTokenExpirationTime = refreshTokenExpirationTime;
-        this.refreshToken = refreshToken;
-        this.accessToken = accessToken;
-    }
-}
-//# sourceMappingURL=refresh-token-info.js.map
