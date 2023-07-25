9f8a98ec8a6cf81c1496c885c5dc52c6700f53e7
Chase Ehlert <chaseehlert123@gmail.com>
2023-07-24 20:55:41 -0400

    fixing lint

--- src/discord/discord-client.ts
+++ src/discord/discord-client.ts
@@ -7,7 +7,6 @@ import { config } from '../../config/config.js'
 import { DestinyApiClient } from '../destiny/destiny-api-client.js'
 import logger from '../utility/logger.js'
 import metaUrl from '../utility/url.js'
-import { fileURLToPath } from 'url'
 
 const userRepo = new MongoUserRepository()
 const destinyService = new DestinyService(new DestinyApiClient())
@@ -41,7 +40,6 @@ export class DiscordClient {
      */
   async setupSlashCommands (discordClient: any): Promise<void> {
     console.log(metaUrl)
-    
     const commandsPath = path.join(metaUrl, '/discord/commands')
     console.log(commandsPath)
     const commandsFiles = fileSystem.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
