f527eb2d612767d6ccfba644b706318d707e4f3d
Chase Ehlert <chaseehlert123@gmail.com>
2024-05-03 13:24:28 -0400

    adding test for destiny api client config class

---
+++ src/infrastructure/destiny/destiny-api-client-config-class.test.ts
@@ -0,0 +1,22 @@
+import { DestinyApiClientConfigClass } from './destiny-api-client-config-class.js'
+
+describe('DestinyApiClientConfigClass', () => {
+  it('should return DestinyApiClientConfigClass from the static fromConfig()', () => {
+    const expectedApiKey = 'key'
+    const expectedOauthSecret = 'secret'
+    const expectedOauthClientId = 'id'
+    const expectedDestinyApiClientConfig = new DestinyApiClientConfigClass(
+      expectedApiKey,
+      expectedOauthSecret,
+      expectedOauthClientId
+    )
+
+    expect(DestinyApiClientConfigClass.fromConfig(
+      {
+        DESTINY_API_KEY: expectedApiKey,
+        DESTINY_OAUTH_SECRET: expectedOauthSecret,
+        DESTINY_OAUTH_CLIENT_ID: expectedOauthClientId
+      }
+    )).toStrictEqual(expectedDestinyApiClientConfig)
+  })
+})
