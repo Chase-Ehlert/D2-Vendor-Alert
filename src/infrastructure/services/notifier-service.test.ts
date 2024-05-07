68e22f201c5ee756f69692e90cfecf006d4def41
Chase Ehlert <chaseehlert123@gmail.com>
2024-05-07 09:07:12 -0400

    refactoring axios call to use http client

--- src/infrastructure/services/notifier-service.test.ts
+++ src/infrastructure/services/notifier-service.test.ts
@@ -1,36 +1,39 @@
 import { NotifierServiceConfig } from './notifier-service-config'
 import { UserInterface } from '../../domain/user'
 import { MongoUserRepository } from '../database/mongo-user-repository'
-import axios from 'axios'
 import { NotifierService } from './notifier-service'
+import { AxiosHttpClient } from '../database/axios-http-client.js'
+import { AxiosResponse } from 'axios'
 
 describe('NotifierService', () => {
-  const mongoUserRepo = new MongoUserRepository()
   const expectedAddress = 'localhost'
+  const mongoUserRepo = new MongoUserRepository()
+  const axiosHttpClient = new AxiosHttpClient()
   const notifierService = new NotifierService(
     mongoUserRepo,
-     { address: expectedAddress } satisfies NotifierServiceConfig
+     { address: expectedAddress } satisfies NotifierServiceConfig,
+     axiosHttpClient
   )
 
   it('should call the notifier service for every user', async () => {
     const userA = { bungieUsername: 'CoolDude' } as unknown as UserInterface
     const userB = { bungieUsername: 'CoolerDude' } as unknown as UserInterface
-
-    mongoUserRepo.fetchAllUsers = jest.fn().mockResolvedValue([userA, userB])
-    axios.post = jest.fn().mockResolvedValue({})
+    const postSpy = jest.spyOn(axiosHttpClient, 'post').mockResolvedValue({} as unknown as AxiosResponse)
+    const fetchAllUsersSpy = jest.spyOn(mongoUserRepo, 'fetchAllUsers').mockResolvedValue([userA, userB])
 
     await notifierService.alertUsersOfUnownedModsForSale()
 
-    expect(axios.post).toHaveBeenCalledWith(
+    expect(fetchAllUsersSpy).toHaveBeenCalled()
+    expect(postSpy).toHaveBeenCalledWith(
       `${expectedAddress}:3002/notify`,
       { user: userA },
       { headers: { 'Content-Type': 'application/json' } }
     )
-    expect(axios.post).toHaveBeenCalledWith(
+    expect(postSpy).toHaveBeenCalledWith(
       `${expectedAddress}:3002/notify`,
       { user: userB },
       { headers: { 'Content-Type': 'application/json' } }
     )
-    expect(axios.post).toBeCalledTimes(2)
+    expect(postSpy).toHaveBeenCalledTimes(2)
   })
 })
--- src/infrastructure/services/notifier-service.ts
+++ src/infrastructure/services/notifier-service.ts
@@ -1,11 +1,12 @@
 import { UserRepository } from '../../domain/user-repository.js'
-import axios from 'axios'
 import { NotifierServiceConfig } from './notifier-service-config.js'
+import { HttpClient } from '../../domain/http-client.js'
 
 export class NotifierService {
   constructor (
     private readonly database: UserRepository,
-    private readonly config: NotifierServiceConfig
+    private readonly config: NotifierServiceConfig,
+    private readonly httpClient: HttpClient
   ) { }
 
   /**
@@ -13,7 +14,7 @@ export class NotifierService {
    */
   async alertUsersOfUnownedModsForSale (): Promise<void> {
     for await (const user of await this.database.fetchAllUsers()) {
-      await axios.post(
+      await this.httpClient.post(
         String(this.config.address).concat(':3002/notify'),
         { user: user },
         { headers: { 'Content-Type': 'application/json' } }
