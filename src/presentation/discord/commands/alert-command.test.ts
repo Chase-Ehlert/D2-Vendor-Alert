2786a5775f5378fc1a14181ff6baf3eae86ca176
Chase Ehlert <chaseehlert123@gmail.com>
2024-05-14 07:31:45 -0400

    testing function argument in AlertCommand

--- src/presentation/discord/commands/alert-command.test.ts
+++ src/presentation/discord/commands/alert-command.test.ts
@@ -1,12 +1,13 @@
+import { hyperlink } from 'discord.js'
 import { AlertCommand } from './alert-command'
 import { AlertCommandConfig } from './alert-command-config.js'
 
 describe('AlertCommand', () => {
-  it('should setup the alert command', () => {
-    const expectedOauthClientId = 'id'
-    const alertConfig = { oauthClientId: expectedOauthClientId } as unknown as AlertCommandConfig
-    const alertCommand = new AlertCommand(alertConfig)
+  const expectedOauthClientId = 'id'
+  const alertConfig = { oauthClientId: expectedOauthClientId } as unknown as AlertCommandConfig
+  const alertCommand = new AlertCommand(alertConfig)
 
+  it('should setup the alert command', () => {
     const command = alertCommand.setupCommand()
 
     expect(command).toMatchObject({
@@ -24,5 +25,28 @@ describe('AlertCommand', () => {
       }),
       execute: expect.any(Function)
     })
+  })
+
+  it('should return a function that creates a hyperlink', () => {
+    const interaction = { followUp: jest.fn() }
+    const interactionFollowUp = (alertCommand as any).interactionFollowUp()
+
+    interactionFollowUp(interaction)
+
+    expect(interaction.followUp).toHaveBeenCalledWith(hyperlink(
+      'Authorize D2 Vendor Alert',
+      `https://www.bungie.net/en/oauth/authorize?client_id=${String(alertConfig.oauthClientId)}&response_type=code`
+    ))
+  })
+
+  it('should throw an error when oauthClientId is undefined', () => {
+    const alertConfig = { oauthClientId: undefined } as unknown as AlertCommandConfig
+    const alertCommand = new AlertCommand(alertConfig)
+    const interaction = { followUp: jest.fn() }
+    const interactionFollowUp = (alertCommand as any).interactionFollowUp()
+
+    try {
+      expect(interactionFollowUp(interaction)).toThrow('OAuth Client Id is undefined for the Alert command!')
+    } catch (error) {}
   })
 })
