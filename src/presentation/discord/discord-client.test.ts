ca99bd2a30201e9efbbe3232336032669a35662d
Chase Ehlert <chaseehlert123@gmail.com>
2024-05-18 14:48:11 -0400

    cleaning up discord-client and adding more testing to it

--- src/presentation/discord/discord-client.test.ts
+++ src/presentation/discord/discord-client.test.ts
@@ -7,6 +7,7 @@ import { AlertCommand } from './commands/alert-command'
 import { DiscordClient } from './discord-client'
 import { AlertCommandConfig } from './commands/alert-command-config.js'
 import * as discord from 'discord.js'
+import { SlashCommand } from '../../domain/slash-command.js'
 
 jest.mock('./../../testing-helpers/url', () => {
   return 'example'
@@ -31,21 +32,29 @@ jest.mock('discord.js', () => ({
   }
 }))
 
+beforeAll(() => {
+  global.console = {
+    ...console,
+    log: jest.fn()
+  }
+})
+
 describe('DiscordClient', () => {
+  const expectedDiscordConfig = { token: 'token' } as unknown as DiscordConfig
+  const mongoUserRepo = new MongoUserRepository()
+  const destinyApiClient = new DestinyApiClient(
+    new AxiosHttpClient(),
+    mongoUserRepo,
+    {} satisfies DestinyApiClientConfig)
+  const alertCommand = new AlertCommand({} satisfies AlertCommandConfig)
+  const discordClient = new DiscordClient(
+    mongoUserRepo,
+    destinyApiClient,
+    alertCommand,
+    expectedDiscordConfig
+  )
+
   it('should setup the Discord client', async () => {
-    const expectedDiscordConfig = { token: 'token' } as unknown as DiscordConfig
-    const mongoUserRepo = new MongoUserRepository()
-    const destinyApiClient = new DestinyApiClient(
-      new AxiosHttpClient(),
-      mongoUserRepo,
-      {} satisfies DestinyApiClientConfig)
-    const alertCommand = new AlertCommand({} satisfies AlertCommandConfig)
-    const discordClient = new DiscordClient(
-      mongoUserRepo,
-      destinyApiClient,
-      alertCommand,
-      expectedDiscordConfig
-    )
     const discordJsClient: any = new discord.Client({
       intents: [
         discord.GatewayIntentBits.Guilds,
@@ -54,12 +63,8 @@ describe('DiscordClient', () => {
         discord.GatewayIntentBits.GuildMessageReactions
       ]
     })
-
-    alertCommand.setupCommand = jest.fn().mockReturnValue(
-      {
-        data: {},
-        execute: {}
-      }
+    jest.spyOn(alertCommand, 'setupCommand').mockReturnValue(
+      { data: {}, execute: {} } as unknown as SlashCommand
     )
 
     await discordClient.setupDiscordClient(discordJsClient)
@@ -67,5 +72,50 @@ describe('DiscordClient', () => {
     expect(discordJsClient.once).toHaveBeenCalledWith(discord.Events.ClientReady, expect.any(Function))
     expect(discordJsClient.login).toHaveBeenCalledWith(expectedDiscordConfig.token)
     expect(discordJsClient.on).toHaveBeenCalledWith(discord.Events.InteractionCreate, expect.any(Function))
+  })
+
+  it('should log that the Discord client is ready', () => {
+    const logDiscordClientIsReady = (discordClient as any).logDiscordClientIsReady()
+    const logSpy = jest.spyOn(console, 'log')
+    const expectedTag = '1234'
+    const eventClient = {
+      user: {
+        tag: expectedTag
+      }
+    }
+
+    logDiscordClientIsReady(eventClient)
+
+    expect(logSpy).toHaveBeenCalledWith(`Ready, logged in as ${eventClient.user.tag}`)
+  })
+
+  it('should reply to slash commands', () => {
+    const command = {} as unknown as SlashCommand
+    const mockMessageCollector = {
+      on: jest.fn()
+    }
+    const interaction: Partial<discord.ChatInputCommandInteraction> = {
+      reply: jest.fn(),
+      valueOf: jest.fn(),
+      isCommand: jest.fn(),
+      client: new discord.Client({} as unknown as discord.ClientOptions),
+      channel: {
+        createMessageCollector: jest.fn().mockReturnValue(mockMessageCollector)
+      } as unknown as discord.TextBasedChannel
+    }
+    const isCommandSpy = jest.spyOn(interaction, 'isCommand').mockReturnValue(true)
+    let getCommandSpy
+    const replySpy = jest.spyOn(interaction, 'reply').mockResolvedValue({} as unknown as discord.InteractionResponse)
+    const handleInteraction = (discordClient as any).handleInteraction()
+
+    if (interaction.client !== undefined) {
+      getCommandSpy = jest.spyOn(interaction.client.commands, 'get').mockReturnValue(command)
+    }
+
+    handleInteraction(interaction)
+
+    expect(isCommandSpy).toHaveBeenCalled()
+    expect(getCommandSpy).toHaveBeenCalled()
+    expect(replySpy).toHaveBeenCalledWith('What is your Bungie Net username? (i.e. "Guardian#1234")')
   })
 })
