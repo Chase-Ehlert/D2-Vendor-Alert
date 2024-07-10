3a86c511524c0f7edd37456b9f98755bfead86cf
Chase Ehlert <chaseehlert123@gmail.com>
2024-07-10 08:23:53 -0400

    fixing destiny client import in vendor

--- src/domain/destiny/vendor.ts
+++ src/domain/destiny/vendor.ts
@@ -1,15 +1,15 @@
+import { DestinyClient } from '../../infrastructure/destiny/destiny-client.js'
 import { UserInterface } from '../user/user.js'
 import { Mod } from './mod.js'
-import { DestinyService } from './destiny-service.js'
 
 export class Vendor {
-  constructor (private readonly destinyService: DestinyService) { }
+  constructor (private readonly destinyClient: DestinyClient) { }
 
   /**
    * Collect mods for sale by Ada-1 that are not owned by the user
    */
   async getUnownedModsForSaleByAda (user: UserInterface): Promise<string[]> {
-    const ownedModHashIds = await this.destinyService.getCollectibleInfo(user.destinyId)
+    const ownedModHashIds = await this.destinyClient.getCollectibleInfo(user.destinyId)
     const modsForSaleByAda = await this.getModsForSaleByAda(user)
     const unownedModsForSaleByAda = modsForSaleByAda.filter((mod) => {
       return !ownedModHashIds.includes(mod.hash)
@@ -22,13 +22,13 @@ export class Vendor {
    * Collect mod info of merchandise for sale by Ada
    */
   private async getModsForSaleByAda (user: UserInterface): Promise<Mod[]> {
-    const adaMerchandise = await this.destinyService.getVendorInfo(
+    const adaMerchandise = await this.destinyClient.getVendorInfo(
       user.destinyId,
       user.destinyCharacterId,
       user.refreshToken
     )
 
-    const equippableMods = await this.destinyService.getDestinyEquippableMods()
+    const equippableMods = await this.destinyClient.getDestinyEquippableMods()
     const unownedModHashes = adaMerchandise.filter(
       (itemHash) =>
         equippableMods.some((mod) => mod.hash === itemHash)
