39c098966e8ef5d7389d17e64bf3ac65d6216baa
Chase Ehlert <chaseehlert123@gmail.com>
2024-09-09 08:03:36 -0400

    adding test coverage for deploy-commands

--- src/presentation/discord/deploy-commands/deploy-commands.test.ts
+++ src/presentation/discord/deploy-commands/deploy-commands.test.ts
@@ -3,19 +3,22 @@ import { AlertCommand } from '../alert-command/alert-command.js'
 import { DeployCommandsConfig } from './deploy-commands-config.js'
 import { DeployCommands } from './deploy-commands.js'
 
+const putResponse = 'response'
+const slashCommand = {
+  data: { key: 'something' }
+}
+let deployCommands: DeployCommands
+let alertCommand: AlertCommand
+
 jest.mock('discord.js', () => ({
   REST: jest.fn().mockImplementation(() => ({
     setToken: jest.fn().mockReturnThis(),
-    put: jest.fn().mockResolvedValue('mockedResponse')
+    put: jest.fn().mockReturnValue(putResponse)
   })),
   Routes: {
     applicationCommands: jest.fn()
   }
 }))
-
-let deployCommands: DeployCommands
-let alertCommand: AlertCommand
-const setupCommandMock = jest.fn()
 
 beforeAll(() => {
   global.console = {
@@ -31,32 +34,17 @@ beforeEach(() => {
     alertCommand
   )
 
-  alertCommand.setupCommand = setupCommandMock
+  alertCommand.setupCommand = jest.fn().mockReturnValue(slashCommand)
 })
 
 describe('DeployCommands', () => {
   it('should register the alert command', async () => {
-    // const slashCommandBuilder: SlashCommandBuilder = jest.fn()
-    // const slashCommand: SlashCommand = {
-    //   data: slashCommandBuilder,
-    //   execute: function (): void {
-    //     throw new Error('Function not implemented.')
-    //   }
-    // }
-
-    // (Routes.applicationCommands as jest.Mock).mockReturnValue('mockCommandRoute')
-
-    const slashCommand = {
-      data: { key: 'something' }
-    }
     const logSpy = jest.spyOn(console, 'log')
-
-    setupCommandMock.mockReturnValue(slashCommand)
 
     await deployCommands.registerCommands()
 
     expect(alertCommand.setupCommand).toHaveBeenCalled()
     expect(logSpy).toHaveBeenCalledWith('Started refreshing the alert application (/) command.')
-    expect(logSpy).toHaveBeenCalledWith('Successfully reloaded 14 application (/) commands.')
+    expect(logSpy).toHaveBeenCalledWith(`Successfully reloaded ${putResponse.length} application (/) commands.`)
   })
 })
