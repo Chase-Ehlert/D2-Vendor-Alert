ba913b491889d62bb9bb371bdb2a909fcf1b2788
Chase Ehlert <chaseehlert123@gmail.com>
2024-09-09 08:58:36 -0400

    Update tests (#38)
    * create test file for deploy command
    
    * initial test file for deploy-commands
    
    * adding test coverage for deploy-commands
    
    * creating deploy-commands-script test
    
    * updating ignore pattern for deploy script

--- src/presentation/discord/deploy-commands/deploy-commands-script.ts
+++ src/presentation/discord/deploy-commands/deploy-commands-script.ts
@@ -11,6 +11,9 @@ const destinyConfig = validateSchema(destinyConfigSchema)
 const DEPLOY_COMMANDS_CONFIG = DeployCommandsConfigClass.fromConfig(discordConfig)
 const ALERT_COMMAND_CONFIG = AlertCommandConfigClass.fromConfig(destinyConfig)
 
-const deployCommands = new DeployCommands(DEPLOY_COMMANDS_CONFIG, new AlertCommand(ALERT_COMMAND_CONFIG))
+const deployCommands = new DeployCommands(
+  DEPLOY_COMMANDS_CONFIG,
+  new AlertCommand(ALERT_COMMAND_CONFIG)
+)
 
 await deployCommands.registerCommands()
